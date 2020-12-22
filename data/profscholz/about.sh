b1637f6a29058ecc8c8b5c955462e6ed810fb82c
David Maus <david.maus@sub.uni-hamburg.de>
2020-12-22 09:23:59 +0100

    Basisadresse Prof. Scholz angepasst

--- data/profscholz/about.sh
+++ data/profscholz/about.sh
@@ -1,3 +1,3 @@
-baseUrl=http://oaigateway.library.ucla.edu/gatewaynet/oai.aspx/www.fzt.haw-hamburg.de/pers/Scholz/Repository1.xml
+baseUrl=https://www.fzt.haw-hamburg.de/pers/Scholz/Repository1.xml
 format=oai_dc
 set=
